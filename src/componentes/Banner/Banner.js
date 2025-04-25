3f80d390daadd4fb30af9f164d30d4678d0144ab
Matheus Alcance <limarios18@outlook.com>
2025-04-25 11:05:53 -0300

    Reconectando ao repositorio

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'; // Importing CSS for styling
+
+function Banner() {
+    // JSX to render the banner image
+  return (
+    <header className="banner">
+        <img src="/img/banner.png" alt="Banner" />
+    </header>
+  );
+}
+
+export default Banner;
