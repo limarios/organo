105d7b89bbdb8992ab40ec6cdb45fcce5c0829e4
Matheus Alcance <limarios18@outlook.com>
2025-04-25 11:51:56 -0300

    adiciona função para deletar colaboradores no componente App

--- src/App.js
+++ src/App.js
@@ -193,6 +193,10 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
+  function deletarColadorador() {
+    console.log('Deletando Colaborador')
+  }
+
 
   return (
     <div>
@@ -200,7 +204,14 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
-        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+        {times.map((time, indice) => 
+          <Time 
+            key={indice} 
+            time={time} 
+            colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
+            aoDeletar={deletarColadorador}
+          />
+        )}
       </section>
       <Rodape />
     </div>
